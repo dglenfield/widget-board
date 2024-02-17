dbbee0e8cfd6c82b2725a50034ff0572af10caef
Danny Glenfield <dglenfield@gmail.com>
2024-02-17 10:47:30 -0500

    Add ability to add widgets to a board along with basic animation when adding widgets #21

---
+++ WidgetBoard/WidgetBoard/Triggers/ShowOverlayTriggerAction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WidgetBoard.Triggers;
+
+public class ShowOverlayTriggerAction : TriggerAction<VisualElement>
+{
+    public bool ShowOverlay { get; set; }
+
+    protected async override void Invoke(VisualElement sender)
+    {
+        if (ShowOverlay) 
+        {
+            sender.Scale = 0;
+            sender.IsVisible = true;
+            sender.Opacity = 0;
+
+            await Task.WhenAll(sender.FadeTo(1), sender.ScaleTo(1, 500, Easing.SpringOut));
+        }
+        else
+        {
+            await sender.ScaleTo(0, 500, Easing.SpringIn);
+
+            sender.Opacity = 0;
+            sender.IsVisible = false;
+        }
+    }
+}
